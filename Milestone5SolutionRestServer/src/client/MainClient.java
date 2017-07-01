c4d59031c37110309ef765a0060cda6b16e01a51
itama <itama@DESKTOP-9USF134>
2017-07-01 23:45:46 +0300

    Final commit

--- Milestone5SolutionRestServer/src/client/MainClient.java
+++ Milestone5SolutionRestServer/src/client/MainClient.java
@@ -1,9 +1,6 @@
 package client;
 
-import java.net.URI;
 import java.util.LinkedList;
-
-import javax.ws.rs.core.UriBuilder;
 
 import commons.Level2D;
 import commons.RestClient;
@@ -12,112 +9,30 @@ import commons.ServerPlan;
 
 public class MainClient {
     public static void main(String[] args) {
-    	//Emulate a client
-//        ClientConfig config = new ClientConfig();
-//        Client client = ClientBuilder.newClient(config);
-//        WebTarget target = client.target(getBaseURI()).path("solution").path("getPlanForLevel");
-//        Invocation.Builder invocationBuilder =  target.request(MediaType.APPLICATION_XML);
-//        Response response = invocationBuilder.post(Entity.entity("STRINGU", MediaType.APPLICATION_XML));
-//        System.out.println(response.getStatus());
-//        System.out.println(response.readEntity(String.class));
-        //End of emulation
-        
-//        RESTclient client=new RESTclient();
-//        TextLevel2DLoader loader=new TextLevel2DLoader();
-//        Level2D level=null;
-//		try {
-//			level = (Level2D)(loader.loadLevelFromStream(new FileInputStream(new File("C:/Users/shefferit/Desktop/ddsd.txt"))));
-//			ServerPlan p=new ServerPlan(new LinkedList<>());
-//			p.setLevelName("SS");
-//			boolean bool=client.sendPlanToDB(p);
-//			System.out.println(bool);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//    	SolutionDBManager dbm=SolutionDBManager.getInstance();
-//		
-//		ServerPlan p=new ServerPlan(new LinkedList<>());
-//		p.setLevelName("SS");
-//		
-//		
-//		Client zc;
-//		ClientConfig config = new ClientConfig();
-//		zc = ClientBuilder.newClient(config);
-//		WebTarget target = zc.target(getBaseURI()).path("planDB").path("putNewPlan");
-//
-//		Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_XML);
-//		Response response = invocationBuilder.put(Entity.entity(p, MediaType.APPLICATION_XML));
-//		if (response.getStatus() == 200) {
-//			System.out.println(true);
-//		} else
-//			System.out.println(false);
-    	//Test for SolutionDBManager
-//		SolutionDBManager dbManager=SolutionDBManager.getInstance();
-//		ServerPlan plan=new ServerPlan(new LinkedList<>());
-//		LinkedList<ServerCommand> commands=new LinkedList<>();
-//		commands.add(new ServerCommand("RIGHT"));
-//		plan.setCommands(commands);
-//		plan.setLevelName("ALALAL");
-//        try {
-//			dbManager.addServerPlan(plan);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//        try {
-//			plan=dbManager.getPlanForLevelName("ISLEVLE");
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//        System.out.println(plan);
-        //End of test
-    	
-    	//Test for RESTclient
+  
     	ServerPlan plan=new ServerPlan(new LinkedList<>());
 		LinkedList<ServerCommand> commands=new LinkedList<>();
-		commands.add(new ServerCommand("NASKH"));
+		commands.add(new ServerCommand("Move right"));
 		plan.setCommands(commands);
-		plan.setLevelName("MANY LMAOS");
+		plan.setLevelName("level 6");
     	RestClient res=RestClient.getInstance();
-    	res.setServerURI("http://localhost:8080/Milestone5SolutionRestServer/");
-    	try {
-			System.out.println(res.sendPlanToDB(plan));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//    	res.setServerURI("http://localhost:8080/Milestone5SolutionRestServer/");
+//    	try {
+//			System.out.println(res.sendPlanToDB(plan));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
     	try {
     		Level2D level=new Level2D();
-    		level.setName("MANY LMAOS");
+    		level.setName("level 3");
 			plan=res.getPlanForLevelName(level.getName());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    	System.out.println(plan.getCommands().get(0).getDescription());
-    	//End of test
+    	for (ServerCommand serverCommand : plan.getCommands()) {
+    		System.out.println(serverCommand.getDescription());
+		}
     	
-//      // Get XML
-//      Todo xmlResponse = target.request()
-//              .accept(MediaType.TEXT_XML).get(Todo.class);
-        
-        // Get XML for application
-//        String xmlAppResponse =target.path("todo").request()
-//                .accept(MediaType.APPLICATION_XML).get(String.class);
 
-        // For JSON response also add the Jackson libraries to your webapplication
-                // In this case you would also change the client registration to
-                // ClientConfig config = new ClientConfig().register(JacksonFeature.class);
-                // Get JSON for application
-                // System.out.println(target.path("rest").path("todo").request()
-                // .accept(MediaType.APPLICATION_JSON).get(String.class));
-
-//        System.out.println(xmlResponse.getDescription());
-//        System.out.println(xmlAppResponse);
     }
-
-    private static URI getBaseURI() {
-        return UriBuilder.fromUri(
-                "http://localhost:8080/Milestone5SolutionRestServer/").build();
-    }
-
 }
--- Milestone5SolutionRestServer/src/services/SolutionService.java
+++ Milestone5SolutionRestServer/src/services/SolutionService.java
@@ -1,14 +1,15 @@
 package services;
 
+import java.util.LinkedList;
+
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import commons.Level2D;
+import commons.ServerCommand;
 import commons.ServerPlan;
 import database.SolutionDBManager;
 
@@ -18,8 +19,129 @@ public class SolutionService {
 	private SolutionDBManager dbManager;
 	public SolutionService() {
 		dbManager=SolutionDBManager.getInstance();
+		startPlan1();
+		startPlan2();
+		startPlan3();
+		startPlan4();
 	}
-
+	private void startPlan1()
+	{
+		LinkedList<ServerCommand> list=new LinkedList<>();
+		ServerCommand c11=new ServerCommand();
+		c11.setDescription("Move right");
+		list.add(c11);
+		ServerCommand c12=new ServerCommand();
+		c12.setDescription("Move right");
+		list.add(c12);
+		ServerPlan plan1= new ServerPlan();
+		plan1.setLevelName("level 1");
+		plan1.setCommands(list);
+		this.putNewPlan(plan1);
+	}
+	
+	private void startPlan2()
+	{
+		LinkedList<ServerCommand> list=new LinkedList<>();
+		ServerCommand c11=new ServerCommand();
+		c11.setDescription("Move right");
+		list.add(c11);
+		ServerCommand c12=new ServerCommand();
+		c12.setDescription("Move right");
+		list.add(c12);
+		ServerCommand c13=new ServerCommand();
+		c13.setDescription("Move right");
+		list.add(c13);
+		ServerCommand c14=new ServerCommand();
+		c14.setDescription("Move right");
+		list.add(c14);
+		ServerCommand c15=new ServerCommand();
+		c15.setDescription("Move right");
+		list.add(c15);
+		ServerCommand c16=new ServerCommand();
+		c16.setDescription("Move up");
+		list.add(c16);
+		ServerPlan plan1= new ServerPlan();
+		plan1.setLevelName("level 2");
+		plan1.setCommands(list);
+		this.putNewPlan(plan1);
+	}
+	
+	
+	
+	private void startPlan3()
+	{
+		LinkedList<ServerCommand> list=new LinkedList<>();
+		ServerCommand c11=new ServerCommand();
+		c11.setDescription("Move up");
+		list.add(c11);
+		ServerCommand c12=new ServerCommand();
+		c12.setDescription("Move left");
+		list.add(c12);
+		ServerCommand c13=new ServerCommand();
+		c13.setDescription("Move up");
+		list.add(c13);
+		ServerCommand c14=new ServerCommand();
+		c14.setDescription("Move right");
+		list.add(c14);
+		ServerPlan plan1= new ServerPlan();
+		plan1.setLevelName("level 3");
+		plan1.setCommands(list);
+		this.putNewPlan(plan1);
+	}
+	private void startPlan4()
+	{
+		LinkedList<ServerCommand> list=new LinkedList<>();
+		ServerCommand c11=new ServerCommand();
+		c11.setDescription("Move up");
+		list.add(c11);
+		ServerCommand c12=new ServerCommand();
+		c12.setDescription("Move right");
+		list.add(c12);
+		ServerCommand c13=new ServerCommand();
+		c13.setDescription("Move right");
+		list.add(c13);
+		ServerCommand c14=new ServerCommand();
+		c14.setDescription("Move right");
+		list.add(c14);
+		ServerCommand c15=new ServerCommand();
+		c15.setDescription("Move up");
+		list.add(c15);
+		ServerCommand c16=new ServerCommand();
+		c16.setDescription("Move right");
+		list.add(c16);
+		ServerCommand c17=new ServerCommand();
+		c17.setDescription("Move right");
+		list.add(c17);
+		ServerCommand c18=new ServerCommand();
+		c18.setDescription("Move down");
+		list.add(c18);
+		ServerCommand c19=new ServerCommand();
+		c19.setDescription("Move up");
+		list.add(c19);
+		ServerCommand c110=new ServerCommand();
+		c110.setDescription("Move left");
+		list.add(c110);
+		ServerCommand c111=new ServerCommand();
+		c111.setDescription("Move left");
+		list.add(c111);
+		ServerCommand c112=new ServerCommand();
+		c112.setDescription("Move down");
+		list.add(c112);
+		ServerCommand c113=new ServerCommand();
+		c113.setDescription("Move down");
+		list.add(c113);
+		ServerCommand c114=new ServerCommand();
+		c114.setDescription("Move right");
+		list.add(c114);
+		ServerCommand c115=new ServerCommand();
+		c115.setDescription("Move up");
+		list.add(c115);
+		ServerPlan plan1= new ServerPlan();
+		plan1.setLevelName("level 4");
+		plan1.setCommands(list);
+		this.putNewPlan(plan1);
+	}
+	
 	@PUT
 	@Path("getPlanForLevel")
 	@Consumes(MediaType.APPLICATION_XML)
